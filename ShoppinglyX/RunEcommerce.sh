5c50727fe02029f778e883ce52061de5cab61b59
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-19 15:57:26 +0530

    chnage sh file

--- ShoppinglyX/RunEcommerce.sh
+++ ShoppinglyX/RunEcommerce.sh
@@ -6,4 +6,4 @@ python3 manage.py createsuperuser --noinput
 
 cd /Ecommerce_django;
 
-gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:run/shoppinglyx.sock
+gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:/Ecommerce_django/run/shoppinglyx.sock
