1ff8eb7b8daa2d9f15352ee4848861c97fbad14a
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-19 15:19:08 +0530

    removing violume mounts insql

--- ShoppinglyX/RunEcommerce.sh
+++ ShoppinglyX/RunEcommerce.sh
@@ -5,4 +5,5 @@ service nginx status
 python3 manage.py createsuperuser --noinput
 
 cd /Ecommerce_django;
+
 gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:run/shoppinglyx.sock
