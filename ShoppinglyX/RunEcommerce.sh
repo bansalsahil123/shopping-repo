2525f152078d9b6f2837fb709bac544295e88854
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-19 16:36:40 +0530

    aaaa

--- ShoppinglyX/RunEcommerce.sh
+++ ShoppinglyX/RunEcommerce.sh
@@ -5,5 +5,4 @@ service nginx status
 python3 manage.py createsuperuser --noinput
 
 cd /Ecommerce_django;
-
 gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:/Ecommerce_django/run/shoppinglyx.sock
