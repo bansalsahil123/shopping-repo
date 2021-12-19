1d5fd987acdd57bb96b1ac7c6191578fe31e55fc
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-19 14:18:31 +0530

    docker build function chhange

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t shopping:latest  -f ./ShoppinglyX/Dockerfile 
-docker build -t shoppingdb:latest  -f ./mysql/Dockerfile 
+docker build -t shopping:latest ./ShoppinglyX/.
+docker build -t shoppingdb:latest ./mysql/.
 
 docker tag shopping:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
 docker tag shoppingdb:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:v1
