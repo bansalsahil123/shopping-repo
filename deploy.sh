bca931afb826a949a3315103743a21f79f054d49
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-20 20:47:05 +0530

    jobbb

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppin
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
 
 kubectl apply -f k8s
-kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  init-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
+kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
 kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
 
