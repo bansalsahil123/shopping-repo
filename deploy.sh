d0f96f2974c74fc0e673b6b51737e081b50f7533
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-20 19:27:13 +0530

    job kube

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,14 @@ docker build -t shoppingdb:latest ./mysql/.
 
 docker tag shopping:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 docker tag shoppingdb:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA
+docker tag shopping:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
 
 
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA
+docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
 
 kubectl apply -f k8s
 kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  init-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
-kubectl set image job/job-ecommerce job-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
+
