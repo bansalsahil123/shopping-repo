f539ec18ecd942d1aac751515a1ef09e96629dd3
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-20 16:35:45 +0530

    deploy set sql

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ docker tag shoppingdb:latest us-central1-docker.pkg.dev/extended-cinema-334708/i
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA
 
-
 kubectl apply -f k8s
 kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  init-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
