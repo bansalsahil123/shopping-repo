9ee58c61d473fb90564f258bcaaef912548a2d46
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-21 14:16:32 +0530

    init added

--- ShoppinglyX/RunEcommerce.sh
+++ ShoppinglyX/RunEcommerce.sh
@@ -1,5 +1,8 @@
 service nginx restart
 service nginx status
 
+
+python3 manage.py createsuperuser --noinput
+
 cd /Ecommerce_django;
 gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:/Ecommerce_django/run/shoppinglyx.sock
