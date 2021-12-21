c5a62dcd0f66eb634b53acff69ff169e84e4b3e7
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-21 13:48:43 +0530

    init added

--- ShoppinglyX/RunEcommerce.sh
+++ ShoppinglyX/RunEcommerce.sh
@@ -1,8 +1,5 @@
 service nginx restart
 service nginx status
 
-sleep 30
-
-
 cd /Ecommerce_django;
 gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:/Ecommerce_django/run/shoppinglyx.sock
--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppin
 kubectl delete -f k8s/pod-init.yaml
 kubectl apply -f k8s
 
-kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
+kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  init-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
 kubectl set image pod/jobpod podjango=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
