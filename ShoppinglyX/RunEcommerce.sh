1c6e8e25bf1c059320aef77624d908f90a353a22
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-21 11:37:27 +0530

    adding pod

--- ShoppinglyX/RunEcommerce.sh
+++ ShoppinglyX/RunEcommerce.sh
@@ -2,7 +2,7 @@ service nginx restart
 service nginx status
 
 sleep 30
-python3 manage.py createsuperuser --noinput
+
 
 cd /Ecommerce_django;
 gunicorn shoppinglyx.wsgi:application --name shoppinglyx --workers 8 --timeout 120 --bind=unix:/Ecommerce_django/run/shoppinglyx.sock
