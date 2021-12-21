afc0bc4c67f211932b77810fea75c84ade36bf1c
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-21 12:05:20 +0530

    scaling pod

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppin
 kubectl apply -f k8s
 kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
 kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
-
+kubectl set image pod/jobpod podjango=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
