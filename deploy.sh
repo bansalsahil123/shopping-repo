e88c6c1ffaed0b3841e4b3cce28161e13c15c147
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-21 13:58:22 +0530

    init added

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppin
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
 
-kubectl delete -f k8s/pod-init.yaml
+#kubectl delete -f k8s/pod-init.yaml
 kubectl apply -f k8s
 
 kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  init-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
 kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
-kubectl set image pod/jobpod podjango=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
+#kubectl set image pod/jobpod podjango=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
