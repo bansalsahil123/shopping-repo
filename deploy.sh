9c38034a4ff8175c6be82c5373e5a76b15b5d0ac
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-23 17:55:36 +0530

    working code with postgress volumes mapping

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 docker build -t shopping:latest ./ShoppinglyX/.
-docker build -t shoppingdb:latest ./mysql/.
+
 
 docker tag shopping:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
-docker tag shoppingdb:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA
 docker tag shopping:latest us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
 
 
 docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
-docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA
-docker push us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:v1
+
+
 
 #kubectl delete -f k8s/pod-init.yaml
 kubectl apply -f k8s
 
 kubectl set image deployments/ecommerce-deployment django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  init-django=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA
-kubectl set image deployments/sql-deployment db=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shoppingdb:$SHA  
-#kubectl set image pod/jobpod podjango=us-central1-docker.pkg.dev/extended-cinema-334708/image-repo/shopping:$SHA  
+
