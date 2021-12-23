8dd489d8b79055700c1833ea9a96f270d8492ebb
bansalsahil123 <bansalsahil227@gmail.com>
2021-12-23 17:18:52 +0530

    adding postgressas a dtabase

--- ShoppinglyX/shoppinglyx/settings.py
+++ ShoppinglyX/shoppinglyx/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'shoppinglyx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-		'NAME': os.environ.get('MYSQL_DATABASES'),
-		'USER': 'local',
-		'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-		'HOST': os.environ.get('MYSQL_HOST'),
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql',
+		'NAME': os.environ.get('PGDATABASE'),
+		'USER': os.environ.get('PGUSER'),
+		'PASSWORD': os.environ.get('PGPASSWORD'),
+		'HOST': os.environ.get('PGHOST'),
+        'PORT': os.environ.get('PGPORT'),
     }
 }
 
